9b931661e516aa1142371d733f245bc249a658d4
Philippe De Pauw - Waterschoot <drdynscript@gmail.com>
2019-12-10 16:48:11 +0100

    Restructured the 11ty app

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ 'site/css': 'css' });
-  eleventyConfig.addPassthroughCopy({ 'site/images': 'images' });
-  eleventyConfig.addPassthroughCopy({ 'site/js': 'js' });
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addPassthroughCopy({ 'site/_static/css': 'static/css' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/images': 'static/images' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/js': 'static/js' });
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('docs/404.html');
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
   return {
     dir: {
@@ -10,8 +33,8 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       data: "_data",
     },
-    pathPrefix: "/",
-    templateFormats: ["html", "liquid", "njk"],
+    pathPrefix: "/11ty-boilerplate",
+    templateFormats: ["html", "liquid", "md", "njk"],
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
